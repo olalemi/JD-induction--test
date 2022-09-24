cbf57a59ce1652d6f4809a07e30bfbef8a366266
olalemi <olaleminurudeen@gmail.com>
2022-09-24 22:34:55 +0100

    Fetched data from API and displayed in browser

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,20 @@
 
 
-fetch("https://www.googleapis.com/books/v1/volumes?q=HTML5")
+ fetch("https://www.googleapis.com/books/v1/volumes?q=HTML5")
 
-  .then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error("NETWORK RESPONSE ERROR");
-    }
-  })
-  .then(data => {
+.then((data) => {
     console.log(data);
-    displayCocktail(data)
-  })
-  .catch((error) => console.error("FETCH ERROR:", error));
-    
-  function displayCocktail(data) {
-    const cocktail = data.items[0];
-    const cocktailDiv = document.getElementById("cocktail");
-    
-  }   
+
+    return data.json();
+
+}).then((collectData) => {
+
+   console.log(collectData.items[2].kind);
+   document.getElementById('featured').
+   innerHTML = collectData.items[2].kind;
+   
+
+})
+
+
+

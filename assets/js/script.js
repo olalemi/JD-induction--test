21f27aa9c74954b3e58edd5bc009613393a490a2
olalemi <olaleminurudeen@gmail.com>
2022-09-25 13:06:58 +0100

    created a featured script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,8 +8,8 @@ fetch('https://www.googleapis.com/books/v1/volumes?q=HTML5')
   .then((collectData) => {
     console.log(collectData.items);
 
-    //    document.getElementById('featured').
-    //    innerHTML = collectData.items[2].kind;
+       document.getElementById('featured').
+       innerHTML = collectData.items[2].kind;
 
     let populate_books = '';
 
@@ -33,3 +33,27 @@ fetch('https://www.googleapis.com/books/v1/volumes?q=HTML5')
 
     document.getElementById('flex-card').innerHTML = populate_books;
   });
+
+
+  
+  let populate_books2 = '';
+
+  let data3 = collectData.items.slice(8,10);
+
+  data3.map((values) => {
+    populate_books2 += `  
+    <div class="featured">
+
+                    <h4>${values.volumeInfo.title} </h4>
+                    <h5>${values.volumeInfo.authors[0]} </h5>
+                    <h6> Pages :${values.volumeInfo.pageCount}</h6>
+                    <p>${values.volumeInfo.description.substr(0,140 )}</p>
+                    <img class="bookstore-logo" src="${values.volumeInfo.imageLinks.thumbnail }" alt="Bookstore-logo" />
+
+
+
+                </div>`;
+  });
+
+  document.getElementById('featured').innerHTML = populate_books2;
+  

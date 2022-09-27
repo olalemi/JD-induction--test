0ce84a75a65e72259f8544f4d894745ae33618b5
olalemi <olaleminurudeen@gmail.com>
2022-09-27 10:35:58 +0100

    updated code to implement onclick events

--- assets/js/featured.js
+++ assets/js/featured.js
@@ -1,51 +1,44 @@
+const featuredBlock = document.getElementById("featured")
+
 fetch('https://www.googleapis.com/books/v1/volumes?q=HTML5')
 
   .then((data) => {
-    // console.log(data);
+    // console.log(data); 
 
     return data.json();
   })
   .then((collectData) => {
-    console.log(collectData.items);
+    // console.log(collectData.items);
 
 
-  let populate_books2 = '';
+    let data2 = collectData.items.slice(8,10);
 
-  let data3 = collectData.items.slice(8,10);
-
-  data3.map((values) => {
-    populate_books2 += ` 
-   
-    <div class="featured">
-
-                         
-                        
-                        <div class="featured-card-items card-items">
-
-                        <h4>${values.volumeInfo.title} </h4>
-                        <h5>${values.volumeInfo.authors[0]} </h5>
-                        <h6> Pages: ${values.volumeInfo.pageCount}</h6>
-                        <p>${values.volumeInfo.description.substr(0,140 )}</p>
-                        <img class="bookstore-logo" src="${values.volumeInfo.imageLinks.thumbnail }" alt="Bookstore-logo" />
-
-                        </div>
-
-                   
-
-                </div>`;
-  });
-
-  document.getElementById('featured').innerHTML = populate_books2;
+    for (let i = 0; i < data2.length; i++) {
+      // let featured = document.createElement("featured");
+      let div = document.createElement("div");
+      div.className = "featured";
+      let c = `
   
+                            
+                              <div class="featured-card-items card-items">
+                              <h4>${data2[i].volumeInfo.title} </h4>
+                              <h5>${data2[i].volumeInfo.authors[0]} </h5>
+                              <h6> Pages: ${data2[i].volumeInfo.pageCount}</h6>
+                              <p>${data2[i].volumeInfo.description.substr(0,140 )}</p>
+                              <img class="bookstore-logo" src="${data2[i].volumeInfo.imageLinks.thumbnail }" alt="Bookstore-logo" />
+                              </div>
+                            
+                        </div>`;
+        div.innerHTML = c;
+        // featured.append(div) ;
+        div.addEventListener("click", () => {
+            console.log("featured " + i);
+        });
+            
+       featuredBlock.append(div);
+       
+    };
 
-  });
 
-
-  
- 
-
-
-
-
-
+  })
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+const flexCard = document.getElementById("flex-card")
+
 fetch('https://www.googleapis.com/books/v1/volumes?q=HTML5')
 
   .then((data) => {
@@ -6,49 +8,33 @@ fetch('https://www.googleapis.com/books/v1/volumes?q=HTML5')
     return data.json();
   })
   .then((collectData) => {
-    console.log(collectData.items);
+    // console.log(collectData.items);
 
-
-    let populate_books = '';
 
     let data2 = collectData.items.slice(0,8);
 
-    data2.map((values) => {
-      populate_books += `
-
-      <div class="card" >
+    for (let i = 0; i < data2.length; i++) {
+      let div = document.createElement("div");
+      div.className = "card";
+      let c = `
+  
     
-                        <img class="bookstore-logo" src="${values.volumeInfo.imageLinks.thumbnail }" alt="Bookstore-logo" />
+                        <img class="bookstore-logo" src="${data2[i].volumeInfo.imageLinks.thumbnail }" alt="Bookstore-logo" />
                         <div class="card-items">
-                            <h4> ${values.volumeInfo.title}</h4>
-                            <h6> ${values.volumeInfo.authors[0]}</h6>
-                            <h6> Pages: ${values.volumeInfo.pageCount}</h6>
-                            <p>${values.volumeInfo.description.substr(0,140 )}</p>
+                            <h4> ${data2[i].volumeInfo.title}</h4>
+                            <h6> ${data2[i].volumeInfo.authors[0]}</h6>
+                            <h6> Pages: ${data2[i].volumeInfo.pageCount}</h6>
+                            <p>${data2[i].volumeInfo.description.substr(0,140 )}</p>
                             
-                        </div>
-
-            </div>`;
-    });
-
-    document.getElementById('flex-card').innerHTML = populate_books;
-
-  });
-
-
-  const selectCard =document.getElementsByClassName("card")
-
-  Array.prototype.forEach.call(selectCard, function(tips) {
-    tips.addEventListener('click', () => {
-    // Do stuff here
-    console.log("yes");
-
-
-  });
-
-  });
-
-
-
-
-
+                        </div>`;
+        div.innerHTML = c;
+        div.addEventListener("click", () => {
+            console.log("card " + i);
+        });
+            
+       flexCard.append(div);
+       
+    };
+        
+  })
 

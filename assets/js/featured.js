94bacfe01c6e30948f46d61215b75fdcf5d5d487
olalemi <olaleminurudeen@gmail.com>
2022-09-29 09:33:14 +0100

    final commit

--- assets/js/featured.js
+++ assets/js/featured.js
@@ -1,80 +1,65 @@
-const featuredBlock = document.getElementById("featured")
+// FeaturedBlock is assigned the returned value  of the element Id whose property matches featured
+const featuredBlock = document.getElementById('featured');
+
+// The fetch API requests the google API for resources and loads it dynamicaly on the webpage, this will return promise.
+// promise here will be either fullfilled or rejected
 
 fetch('https://www.googleapis.com/books/v1/volumes?q=HTML5')
-
   .then((data) => {
-    // console.log(data); 
-
     return data.json();
   })
   .then((collectData) => {
-    // console.log(collectData.items);
-
-
-    let data2 = collectData.items.slice(8,10);
-
+    // Slice method returns the last two books from the 10 books in google API books
+    let data2 = collectData.items.slice(8, 10);
     for (let i = 0; i < data2.length; i++) {
-      // let featured = document.createElement("featured");
-      let div = document.createElement("div");
-      div.className = "featured";
-      let c = `
-  
-                            
+      //  created a div element(div) and assigned a dynamic class (featured)  
+      let div = document.createElement('div');
+      div.className = 'featured';
+      let content = `
                               <div class="featured-card-items card-items">
                               <h4>${data2[i].volumeInfo.title} </h4>
                               <h5>${data2[i].volumeInfo.authors[0]} </h5>
                               <h6> Pages: ${data2[i].volumeInfo.pageCount}</h6>
-                              <p>${data2[i].volumeInfo.description.substr(0,140 )}</p>
-                              <img class="bookstore-logo" src="${data2[i].volumeInfo.imageLinks.thumbnail }" alt="Bookstore-logo" />
+                              <p>${data2[i].volumeInfo.description.substr(0,140)}</p>
+                              <img class="bookstore-logo" src="${data2[i].volumeInfo.imageLinks.thumbnail}" alt="Bookstore-logo" />
                               </div>
                             
                         </div>`;
-        div.innerHTML = c;
-        // featured.append(div) ;
-        div.addEventListener("click", () => {
-            console.log("featured " + i);
-        });
-            
-       featuredBlock.append(div);
-       
-    };
+      div.innerHTML = content;
+      // div was appended to the featuredBlock Id
+      featuredBlock.append(div);
+    }
+    // retrieve selectedItems  if it exists. If it doesn't create an empty array
+    let selectedItems =JSON.parse(localStorage.getItem('featuredSelectedItem')) || [];
 
+    // FeaturedBooks is assigned the returned value  of the element classname whose property matches string(featured)
+    let featuredBooks = document.getElementsByClassName('featured');
 
+    for (let i = 0; i < featuredBooks.length; i++) {
+      // after reload if  local Storage includes variable i the function adds class is-selected to the selected item fom the array
 
-    
-    let selectedItems = JSON.parse(localStorage.getItem("featuredSelectedItem")) || [];
+      if (selectedItems.includes(i)) {
+        featuredBooks[i].classList.add('is-selected');
+      }
 
-    //   console.log(selectedItems)
-    
-      setTimeout(() => {
-        
-        selectedItems.forEach((selectedItem) => {
-            document.getElementById(`${selectedItem}`).classList.add("is-selected");
-        });
-    }, 500);
-    
-            let fbookss = document.getElementsByClassName("featured");
-                for (let i = 0; i < fbookss.length; i++) {
-                    fbookss[i].addEventListener("click", () => {
-                        const classes = fbookss[i].classList;
-                        if (classes.contains("is-selected")) {
-                            /* If fbookss is selected through the class, then remove the is-selected class and also remove from localstorage */
-                            fbookss[i].classList.remove("is-selected");
-                            // remove the selected item from the array
-                            selectedItems = selectedItems.filter((item) => item !== fbookss[i].id);
-                        } else {
-                            /* This Stores the selected book's ID into localstorage via is-selected class */
-                            fbookss[i].classList.add("is-selected");
-                            // add the selected item to the array
-                            selectedItems.push(fbookss[i].id);
-                        }
-                        // store the updated selectedItems array in localStorage as json
-                        localStorage.setItem("featuredSelectedItem", JSON.stringify(selectedItems));
-                    });
-                }
-          
-              
-              
+      featuredBooks[i].addEventListener('click', () => {
+        const classes = featuredBooks[i].classList;
 
+        console.log(selectedItems.includes(i));
+
+        // onclick, if featuredBook class contains class is-selected,  is-selected is removed from the selectedItem and  the local storage
+
+        if (classes.contains('is-selected')) {
+          featuredBooks[i].classList.remove('is-selected');
+          selectedItems = selectedItems.filter((item) => item !== i);
+
+          // if the above is false, a class is-slected is added  and selectedItem added to array local storage
+        } else {
+          featuredBooks[i].classList.add('is-selected');
+          selectedItems.push(i);
+        }
+        //  updates selectedItems in local storage array as json
+        localStorage.setItem('featuredSelectedItem', JSON.stringify(selectedItems));
+      });
+    }
   });
-
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,77 +1,65 @@
-const flexCard = document.getElementById("flex-card")
+// Flexcard is assigned the returned value  of the element Id whose property matches flex card()
+const flexCard = document.getElementById('flex-card');
+
+// The fetch API requests the google API for resources and loads it dynamicaly on the webpage, this will return promise.
+// promise here will be either fullfilled or rejected
 
 fetch('https://www.googleapis.com/books/v1/volumes?q=HTML5')
-
   .then((data) => {
-    // console.log(data);
-
     return data.json();
   })
   .then((collectData) => {
-    // console.log(collectData.items);
-
-
-    let data2 = collectData.items.slice(0,8);
-
+// Slice method returns the first 8books from the 10 books in google API books
+    let data2 = collectData.items.slice(0, 8);
     for (let i = 0; i < data2.length; i++) {
-      let div = document.createElement("div");
-      div.className = "card";
-      let c = `
-  
-    
-                        <img class="bookstore-logo" src="${data2[i].volumeInfo.imageLinks.thumbnail }" alt="Bookstore-logo" />
+        //  created a div element(div) and assigned a dynamic class (card)  
+      let div = document.createElement('div');
+      div.className = 'card';
+      let content = `
+                        <img class="bookstore-logo" src="${ data2[i].volumeInfo.imageLinks.thumbnail }" alt="Bookstore-logo">
                         <div class="card-items">
                             <h4> ${data2[i].volumeInfo.title}</h4>
                             <h6> ${data2[i].volumeInfo.authors[0]}</h6>
                             <h6> Pages: ${data2[i].volumeInfo.pageCount}</h6>
-                            <p>${data2[i].volumeInfo.description.substr(0,140 )}</p>
+                            <p>${data2[i].volumeInfo.description.substr( 0,140)}</p>
                             
                         </div>`;
-        div.innerHTML = c;
-        div.addEventListener("click", () => {
-            console.log("card " + i);
-        });
-            
-       flexCard.append(div);
-       
-    };
+      div.innerHTML = content;
+      // div was appended to the flexcard Id
+      flexCard.append(div);
+    }
 
-
-    let selectedItems = JSON.parse(localStorage.getItem("cardSelectedItem")) || [];
-
-    //   console.log(selectedItems)
+    let selectedItems =JSON.parse(localStorage.getItem('cardSelectedItem')) || [];
     
-      setTimeout(() => {
-        
-        selectedItems.forEach((selectedItem) => {
-            document.getElementById(`${selectedItem}`).classList.add("is-selected");
-        });
-    }, 500);
-    
-            let fBooks = document.getElementsByClassName("card");
-            
-                for (let i = 0; i < fBooks.length; i++) {
-                    fBooks[i].addEventListener("click", () => {
-                        const classes = fBooks[i].classList;
-                        if (classes.contains("is-selected")) {
-                            /* If fbooks is selected through the class, then remove the is-selected class and also remove from localstorage */
-                            fBooks[i].classList.remove("is-selected");
-                            // remove the selected item from the array
-                            selectedItems = selectedItems.filter((item) => item !== fBooks[i].id);
-                        } else {
-                            /* This Stores the selected book's ID into localstorage via is-selected class */
-                            fBooks[i].classList.add("is-selected");
-                            // add the selected item to the array
-                            selectedItems.push(fBooks[i].id);
-                        }
-                        // store the updated selectedItems array in localStorage as json
-                        localStorage.setItem("cardSelectedItem", JSON.stringify(selectedItems));
-                    });
-                }
-          
-               
-                 
-  });
+ // FeaturedBooks is assigned the returned value  of the element classname whose property matches string(card)
+ let cardBooks = document.getElementsByClassName('card');
 
+ for (let i = 0; i < cardBooks.length; i++) {
+   // after reload if  local Storage includes variable i the function adds class is-selected to the selected item fom the array
 
-        
+   if (selectedItems.includes(i)) {
+     cardBooks[i].classList.add('is-selected');
+   }
+
+   cardBooks[i].addEventListener('click', () => {
+     const classes = cardBooks[i].classList;
+
+     console.log(selectedItems.includes(i));
+
+     // onclick, if cardBooks class contains class is-selected,  is-selected is removed from the selectedItem and  the local storage
+
+     if (classes.contains('is-selected')) {
+       cardBooks[i].classList.remove('is-selected');
+       selectedItems = selectedItems.filter((item) => item !== i);
+
+       // if the above is false, a class is-slected is added  and selectedItem added to array local storage
+     } else {
+       cardBooks[i].classList.add('is-selected');
+       selectedItems.push(i);
+     }
+     //  updates selectedItems in local storage array as json
+
+     localStorage.setItem( 'cardSelectedItem', JSON.stringify(selectedItems));
+   });
+ }
+});
